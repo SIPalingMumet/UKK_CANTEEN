25e3cd6864df705a947726d29760392fb119864a
SiPalingMumet <moch_ardhiansah_31rpl@student.smktelkom-mlg.sch.id>
2025-01-30 14:11:11 +0700

    splash unfinished

---
+++ lib/LandingPage.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class Landingpage extends StatefulWidget {
+  const Landingpage({super.key});
+
+  @override
+  State<Landingpage> createState() => _LandingpageState();
+}
+
+class _LandingpageState extends State<Landingpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/Splash_Screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:ukk_kantin_2/LandingPage.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({super.key});
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => const Landingpage()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.blue,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'E-CANTEEN',
+              style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(height: 20),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
